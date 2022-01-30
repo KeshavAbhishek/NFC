bd62cc5a384b052e96cd4109cdbd502bdadc93e8
Cool_Tkinter_Projects <crystaled2003@gmail.com>
2022-01-30 23:01:58 +0530

    NFCUpgraded

--- functionalityUpgraded.js
+++ functionalityUpgraded.js
@@ -235,5 +235,30 @@ function onPrint(){
     document.getElementById('calculateAmount').innerText='THANK YOU !';
     document.getElementById('calculateAmount').style.cursor='none';
 
-    print()
-}
+    var fromdatevalue=document.getElementById('fromdate').value;
+    var todatevalue=document.getElementById('todate').value;
+
+    fromdatevalue=fromdatevalue.split('-')
+    fromdatevalue=fromdatevalue.reverse()
+    fromdatevalue=fromdatevalue.join('-')
+
+    todatevalue=todatevalue.split('-')
+    todatevalue=todatevalue.reverse()
+    todatevalue=todatevalue.join('-')
+
+    document.getElementById('fromdate').style.display='none';
+    document.getElementById('todate').style.display='none';
+
+    document.getElementById('FROMDATE').style.width="100%";
+    document.getElementById('TODATE').style.width="100%";
+    document.getElementById('FROMDATE').style.textAlign="center";
+    document.getElementById('TODATE').style.textAlign="center";
+    document.getElementById('FROMDATE').innerText+=' '+fromdatevalue;
+    document.getElementById('TODATE').innerText+=' '+todatevalue;
+
+    setTimeout(() => {
+        html2canvas(document.getElementById('mainFrame')).then(canvas=>{
+            document.getElementById('frame-9').appendChild(canvas);
+        });
+    }, 2000);
+};
